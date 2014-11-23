b5400b1fa324d981bde6928a8a3910d7b104208b
Socialz <socialz.fin@gmail.com>
2014-11-23 23:15:17 +0200

    database: added database system

---
+++ database/server/database.lua
@@ -0,0 +1,170 @@
+﻿database = { }
+database.queue = { }
+
+database.configuration = { }
+database.configuration.database_type = get( "connection_type" ) or "mysql"
+database.configuration.database_file = get( "database_file" ) or "database"
+database.configuration.database_batch = get( "database_batch" ) or 0
+database.configuration.database_log = get( "database_log" ) or 1
+database.configuration.database_tag = get( "database_tag" ) or "script"
+
+database.configuration.hostname = get( "hostname" ) or "127.0.0.1"
+database.configuration.username = get( "username" ) or "root"
+database.configuration.password = get( "password" ) or "251096pmj"
+database.configuration.database = get( "database" ) or "fairplay_rp"
+
+database.connection = nil
+
+local patterns = {
+	-- gsub pattern, strip double/triple/... whitespaces
+	[ "all_no_double" ] 		= { "[^%a%d%s%.%-%_%:%,%;]", true },
+	[ "all" ] 					= { "[^%a%d%s%.%-%_%:%,%;]" },
+	[ "account" ]				= { "[^%a%d%.%-%_%!%?]" },
+	[ "character" ]				= { "[^%a%d%s%-]" },
+	[ "digit" ] 				= { "[^%d]" },
+	[ "char_digit" ] 			= { "[^%a%d]" },
+	[ "char_digit_special" ]	= { "[^%a%d%_]" },
+	[ "less_no_double" ] 		= { "[^%a%d%s]", true },
+	[ "less" ] 					= { "[^%a%d%s]" },
+}
+
+local function connect( )
+	database.queue.connection = dbConnect( database.configuration.database_type, ( database.configuration.database_type == "sqlite" and database.configuration.database_file or "dbname=" .. database.configuration.database .. ";host=" .. database.configuration.hostname ), ( database.configuration.database_type == "sqlite" and "" or database.configuration.username ), ( database.configuration.database_type == "sqlite" and "" or database.configuration.password ), "share=1;batch=" .. database.configuration.database_batch .. ";log=" .. database.configuration.database_log .. ";tag=" .. database.configuration.database_tag )
+	
+	if ( database.queue.connection ) then
+		database.connection = database.queue.connection
+		database.queue.connection = nil
+		outputDebugString( "DATABASE: Database connection initialized." )
+		
+		return database.connection
+	end
+	
+	outputDebugString( "DATABASE: Database connection could not be initialized.", 2 )
+	
+	return false
+end
+addEventHandler( "onResourceStart", resourceRoot, connect )
+
+function disconnect( queueRestart )
+	if ( database.connection ) then
+		destroyElement( database.connection )
+		database.connection = nil
+		
+		outputDebugString( "DATABASE: Database connection destroyed." )
+		
+		if ( queueRestart == true ) then
+			outputDebugString( "DATABASE: Database connection restart pending." )
+			connect( )
+		end
+		
+		return true
+	end
+	
+	outputDebugString( "DATABASE: Database connection is not alive and could not be destroyed.", 2 )
+	
+	return false
+end
+addEventHandler( "onResourceStop", resourceRoot, disconnect )
+
+function ping( )
+	if ( database.connection ) and ( query( "SELECT 1" ) ) then return true end
+	return false
+end
+
+function query( queryString, ... )
+	if ( not queryString ) then
+		outputDebugString( "DATABASE: Database query string missing.", 1 )
+		return false, 1
+	end
+	
+	if ( database.connection ) then
+		local query = (...) and dbQuery( database.connection, queryString, ... ) or dbQuery( database.connection, queryString )
+		
+		if ( query ) then
+			local result, num_affected_rows, last_insert_id = dbPoll( query, -1 )
+			
+			if ( result == false ) then
+				local error_code, error_msg = num_affected_rows, last_insert_id
+				
+				dbFree( query )
+				outputDebugString( "DATABASE: Database query failed - (errno " .. error_code .. "; error: " .. error_msg .. ").", 1 )
+				
+				return false
+			else
+				return result, num_affected_rows, last_insert_id
+			end
+		end
+	end
+	
+	return false
+end
+
+function query_single( queryString, ... )
+	local result, num_affected_rows, last_insert_id = query( queryString, ... )
+	
+	if ( result ) and ( num_affected_rows > 0 ) then
+		return result[ 1 ] or result, num_affected_rows, last_insert_id
+	end
+	
+	return false
+end
+
+function execute( queryString, ... )
+	local parameters = { ... }
+	local possibleHandler = parameters[ 1 ]
+	
+	if ( not queryString ) then
+		outputDebugString( "DATABASE: Database query string missing.", 1 )
+		return false, 1
+	end
+	
+	if ( database.connection ) then
+		local query = (...) and dbExec( database.connection, queryString, ... ) or dbExec( database.connection, queryString )
+		
+		if ( query ) then
+			return true
+		end
+	end
+	
+	return false
+end
+
+function insert_id( queryString, ... )
+	local result, _, last_insert_id = query( queryString, ... )
+	
+	if ( result ) then
+		return last_insert_id
+	end
+	
+	return false
+end
+
+function free_result( queryHandler )
+	if ( not queryHandler ) then
+		outputDebugString( "DATABASE: Database query handler missing.", 1 )
+		return false, 1
+	end
+	
+	if ( dbFree( queryHandler ) ) then
+		return true
+	end
+	
+	return false
+end
+
+function escape_string( string, caution )
+	if ( not string ) then
+		outputDebugString( "DATABASE: String-to-be-escaped is missing.", 1 )
+		return false, 1
+	end
+	
+	local string, caution = tostring( string ), ( ( caution and patterns[ caution ] ) and caution or "all_no_double" )
+	
+	if ( patterns[ caution ][ 2 ] ) then
+		while string:find( "  " ) do
+			string = string:gsub( "  ", " " )
+		end
+	end
+	
+	return ( tonumber( tostring( string:gsub( patterns[ caution ][ 1 ], "" ) ) ) and tonumber( tostring( string:gsub( patterns[ caution ][ 1 ], "" ) ) ) or tostring( string:gsub( patterns[ caution ][ 1 ], "" ) ) )
+end
---
+++ database/server/verification.lua
@@ -0,0 +1,112 @@
+database.configuration.automated_resources = { accounts = { "accounts" }, vehicles = { "vehicles" } }
+database.configuration.default_charset = get( "default_charset" ) or "utf8"
+database.configuration.default_engine = get( "default_engine" ) or "InnoDB"
+database.utility = { }
+database.verification = {
+	-- name, type, length, default, is_unsigned, is_null, is_auto_increment, key_type
+	accounts = {
+		{ name = "id", type = "int", length = 10, is_unsigned = true, is_auto_increment = true, key_type = "primary" },
+		{ name = "username", type = "varchar", length = 25, default = "" },
+		{ name = "password", type = "varchar", length = 1000, default = "" },
+		{ name = "rank", type = "tinyint", length = 3, default = 0 },
+		{ name = "is_deleted", type = "tinyint", length = 1, default = 0 },
+		{ name = "last_login", type = "datetime", default = "0000-00-00 00:00:00" },
+		{ name = "last_action", type = "datetime", default = "0000-00-00 00:00:00" },
+		{ name = "last_ip", type = "varchar", length = 128, default = "0.0.0.0" },
+		{ name = "last_serial", type = "varchar", length = 32, default = "13371337133713371337133713371337" }
+	},
+	vehicles = {
+		{ name = "id", type = "int", length = 10, is_unsigned = true, is_auto_increment = true, key_type = "primary" },
+		{ name = "model_id", type = "smallint", length = 3, default = 0, is_unsigned = true },
+		{ name = "pos_x", type = "float", default = 0 },
+		{ name = "pos_y", type = "float", default = 0 },
+		{ name = "pos_z", type = "float", default = 0 },
+		{ name = "rot_x", type = "float", default = 0 },
+		{ name = "rot_y", type = "float", default = 0 },
+		{ name = "rot_z", type = "float", default = 0 },
+		{ name = "interior", type = "tinyint", length = 3, default = 0, is_unsigned = true },
+		{ name = "dimension", type = "smallint", length = 5, default = 0, is_unsigned = true },
+		{ name = "respawn_pos_x", type = "float", default = 0 },
+		{ name = "respawn_pos_y", type = "float", default = 0 },
+		{ name = "respawn_pos_z", type = "float", default = 0 },
+		{ name = "respawn_rot_x", type = "float", default = 0 },
+		{ name = "respawn_rot_y", type = "float", default = 0 },
+		{ name = "respawn_rot_z", type = "float", default = 0 },
+		{ name = "respawn_interior", type = "tinyint", length = 3, default = 0, is_unsigned = true },
+		{ name = "respawn_dimension", type = "smallint", length = 5, default = 0, is_unsigned = true },
+		{ name = "numberplate", type = "varchar", length = 10, default = "UNd3F1N3D" },
+		{ name = "variant_1", type = "tinyint", length = 3, default = 255, is_unsigned = true },
+		{ name = "variant_2", type = "tinyint", length = 3, default = 255, is_unsigned = true },
+		{ name = "account_id", type = "int", length = 11, default = 0, is_unsigned = true },
+		{ name = "group_id", type = "int", length = 11, default = 0, is_unsigned = true },
+		{ name = "health", type = "smallint", length = 4, default = 1000, is_unsigned = true },
+		{ name = "color", type = "varchar", length = 255, default = "[ [ 0, 0, 0 ], [ 0, 0, 0 ], [ 0, 0, 0], [ 0, 0, 0 ] ]" },
+		{ name = "headlight_color", type = "varchar", length = 255, default = "[ [ 0, 0, 0 ] ]" },
+		{ name = "headlight_state", type = "tinyint", length = 1, default = 0, is_unsigned = true },
+		{ name = "door_states", type = "varchar", length = 255, default = "[ [ 0, 0, 0, 0, 0, 0 ] ]" },
+		{ name = "panel_states", type = "varchar", length = 255, default = "[ [ 0, 0, 0, 0, 0, 0 ] ]" },
+		{ name = "is_locked", type = "tinyint", length = 1, default = 1, is_unsigned = true },
+		{ name = "is_engine_on", type = "tinyint", length = 1, default = 0, is_unsigned = true },
+		{ name = "is_deleted", type = "tinyint", length = 1, default = 0, is_unsigned = true },
+		{ name = "is_broken", type = "tinyint", length = 1, default = 0, is_unsigned = true },
+		{ name = "is_bulletproof", type = "tinyint", length = 1, default = 0, is_unsigned = true },
+		{ name = "created_time", type = "datetime", default = "0000-00-00 00:00:00" },
+		{ name = "created_by", type = "int", length = 11, default = 0, is_unsigned = true }
+	}
+}
+
+database.utility.keys = { unique = true, primary = true, index = true }
+function getFormattedKeyType( keyValue, keyType )
+	if ( keyValue ) and ( database.utility.keys[ keyType ] ) then
+		return "\r\n" .. ( keyType ~= "index" and keyType:upper( ) .. " " or "" ) .. "KEY (`" .. keyValue .. "`),"
+	end
+	return ""
+end
+
+function verify_table( tableName )
+	local tableName = escape_string( tableName, "char_digit_special" )
+	if ( tableName ) and ( database.verification[ tableName ] ) then
+		local query = query( "SELECT 1 FROM `" .. tableName .. "`" )
+		if ( query ) then
+			return true, 0
+		else
+			outputDebugString( "DATABASE: Don't mind the warning messages above; verify_table is running right now." )
+			
+			local query_string = "CREATE TABLE IF NOT EXISTS `" .. tableName .. "` ("
+			
+			for columnID, columnData in ipairs( database.verification[ tableName ] ) do
+				query_string = query_string .. "\r\n`" .. columnData.name .. "` " .. columnData.type .. ( columnData.length and "(" .. columnData.length .. ")" or "" ) .. ( columnData.is_unsigned and " unsigned" or "" ) .. " " .. ( columnData.is_null and "NULL" or "NOT NULL" ) .. ( columnData.default and " DEFAULT '" .. columnData.default .. "'" or "" ) .. ( columnData.is_auto_increment and " AUTO_INCREMENT" or "" ) .. ( #database.verification[ tableName ] ~= columnID and "," or "" ) .. getFormattedKeyType( columnData.name, columnData.key_type )
+			end
+			
+			query_string = query_string .. "\r\n) ENGINE=" .. database.configuration.default_engine .. " DEFAULT CHARSET=" .. database.configuration.default_charset .. ";"
+			
+			if ( execute( query_string ) ) then
+				outputDebugString( "DATABASE: Created table '" .. tableName .. "'." )
+				return true, 2
+			else
+				outputDebugString( "DATABASE: Unable to create table '" .. tableName .. "'.", 2 )
+				return false, 2
+			end
+			
+			return false
+		end
+	end
+	return false, 1
+end
+
+addEventHandler( "onResourcePreStart", root,
+	function( resource )
+		if ( database.configuration.automated_resources[ getResourceName( resource ) ] ) then
+			outputDebugString( "DATABASE: Verification check will be ran on just started '" .. getResourceName( resource ) .. "' resource." )
+			
+			for _,database in ipairs( database.configuration.automated_resources[ getResourceName( resource ) ] ) do
+				local _return, _code = verify_table( database )
+				if ( _return ) and ( _code > 0 ) then
+					outputDebugString( "DATABASE: Verification check completed for \"" .. database .. "\": database created." )
+				else
+					outputDebugString( "DATABASE: Verification check completed for \"" .. database .. "\": database wasn't created, because it already exists, most probably." )
+				end
+			end
+		end
+	end
+)
