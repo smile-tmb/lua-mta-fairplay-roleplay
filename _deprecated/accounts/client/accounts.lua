075c893a9efaea391dd86d4110fcd5b3f6551658
Socialz <socialz.fin@gmail.com>
2014-11-24 21:35:33 +0200

    deprecated: added some cool script

--- _deprecated/accounts/client/accounts.lua
+++ _deprecated/accounts/client/accounts.lua
@@ -1,3 +1,43 @@
+--[[
+-- cool script
+local loremIpsumWords = { 'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'curabitur', 'vel', 'hendrerit', 'libero', 'eleifend', 'blandit', 'nunc', 'ornare', 'odio', 'ut', 'orci', 'gravida', 'imperdiet', 'nullam', 'purus', 'lacinia', 'a', 'pretium', 'quis', 'congue', 'praesent', 'sagittis', 'laoreet', 'auctor', 'mauris', 'non', 'velit', 'eros', 'dictum', 'proin', 'accumsan', 'sapien', 'nec', 'massa', 'volutpat', 'venenatis', 'sed', 'eu', 'molestie', 'lacus', 'quisque', 'porttitor', 'ligula', 'dui', 'mollis', 'tempus', 'at', 'magna', 'vestibulum', 'turpis', 'ac', 'diam', 'tincidunt', 'id', 'condimentum', 'enim', 'sodales', 'in', 'hac', 'habitasse', 'platea', 'dictumst', 'aenean', 'neque', 'fusce', 'augue', 'leo', 'eget', 'semper', 'mattis', 'tortor', 'scelerisque', 'nulla', 'interdum', 'tellus', 'malesuada', 'rhoncus', 'porta', 'sem', 'aliquet', 'et', 'nam', 'suspendisse', 'potenti', 'vivamus', 'luctus', 'fringilla', 'erat', 'donec', 'justo', 'vehicula', 'ultricies', 'varius', 'ante', 'primis', 'faucibus', 'ultrices', 'posuere', 'cubilia', 'curae', 'etiam', 'cursus', 'aliquam', 'quam', 'dapibus', 'nisl', 'feugiat', 'egestas', 'class', 'aptent', 'taciti', 'sociosqu', 'ad', 'litora', 'torquent', 'per', 'conubia', 'nostra', 'inceptos', 'himenaeos', 'phasellus', 'nibh', 'pulvinar', 'vitae', 'urna', 'iaculis', 'lobortis', 'nisi', 'viverra', 'arcu', 'morbi', 'pellentesque', 'metus', 'commodo', 'ut', 'facilisis', 'felis', 'tristique', 'ullamcorper', 'placerat', 'aenean', 'convallis', 'sollicitudin', 'integer', 'rutrum', 'duis', 'est', 'etiam', 'bibendum', 'donec', 'pharetra', 'vulputate', 'maecenas', 'mi', 'fermentum', 'consequat', 'suscipit', 'aliquam', 'habitant', 'senectus', 'netus', 'fames', 'quisque', 'euismod', 'curabitur', 'lectus', 'elementum', 'tempor', 'risus', 'cras' }
+local tipBox = { string = "" }
+local boxX, boxY = 50 * screenWidth / screenWidth, 50 * screenHeight / screenHeight
+local boxPadding = 15
+local lineHeight = dxGetFontHeight( 1, "default-bold" )
+local minimumWidth = 350
+local offsetWidth = 25
+
+addEventHandler( "onClientRender", root,
+	function( )
+		tipBox.string = tipBox.string .. loremIpsumWords[ math.random( #loremIpsumWords ) ] .. " "
+		
+		if ( tipBox.string:len( ) > 2000 ) then
+			tipBox.string = ""
+		end
+		
+		local lines = 0
+		local wordbreak = false
+		local lineWidth = dxGetTextWidth( tipBox.string, 1, "default-bold" )
+		
+		while ( lineWidth + offsetWidth > minimumWidth ) do
+			lineWidth = lineWidth - minimumWidth
+			lines = lines + 1
+			wordbreak = true
+		end
+		
+		local boxWidth, boxHeight = minimumWidth + ( boxPadding * 3 ), ( lineHeight * ( lines + 1 ) ) + ( boxPadding * 2 )
+		
+		dxDrawRectangle( boxX, boxY, boxWidth, boxHeight, tocolor( 0, 0, 0, 185 ), true )
+		
+		local textX, textY = boxX + boxPadding, boxY + boxPadding
+		local textWidth, textHeight = textX + minimumWidth + boxPadding, textY + lineHeight + boxPadding
+		
+		dxDrawText( tipBox.string, textX, textY, textWidth, textHeight, tocolor( 222, 222, 222, 255 ), 1, "default-bold", "left", "top", false, wordbreak, true )
+	end
+)
+]]
+
 accounts_login_view = {
 	button = { },
 	label = { },
