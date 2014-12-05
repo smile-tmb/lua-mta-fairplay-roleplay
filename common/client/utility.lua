36376ddf9a90bc2aba7cf551a6f5054de32c181b
Socialz <socialz.fin@gmail.com>
2014-12-05 16:39:24 +0200

    common: fixed utility problem

--- common/client/utility.lua
+++ common/client/utility.lua
@@ -1,20 +1,10 @@
 local _addCommandHandler = addCommandHandler
-function addCommandHandler( commandName, handlerFunction, restricted, caseSensitive )
+function addCommandHandler( commandName, handlerFunction, caseSensitive )
 	if ( type( commandName ) ~= "table" ) then
 		commandName = { commandName }
 	end
 	
 	for key, value in ipairs( commandName ) do
-		if ( key == 1 ) then
-			_addCommandHandler( value, handlerFunction, restricted, caseSensitive )
-		else
-			_addCommandHandler( value,
-				function( player, ... )
-					if ( hasObjectPermissionTo( player, "command." .. commandName[ 1 ], not restricted ) ) then
-						handlerFunction( player, ... )
-					end
-				end
-			)
-		end
+		_addCommandHandler( value, handlerFunction, caseSensitive )
 	end
 end
