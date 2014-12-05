b4555d340d0fc85e4cdfb149bf3b85547ca3333a
Socialz <socialz.fin@gmail.com>
2014-12-05 16:40:43 +0200

    realism: add strafing ability with q & e keys

---
+++ realism/client/strafe.lua
@@ -0,0 +1,23 @@
+addEventHandler( "onClientPreRender", root,
+	function( )
+		
+		local leftPressed = getKeyState( "q" )
+		local rightPressed = getKeyState( "e" )
+		
+		if ( ( leftPressed ) or ( rightPressed ) ) and ( not isMTAWindowActive( ) ) then
+			setControlState( "forwards", true )
+			--setControlState( "sprint", true )
+			
+			if ( leftPressed ) then
+				setControlState( "left", true )
+			elseif ( rightPressed ) then
+				setControlState( "right", true )
+			end
+		else
+			setControlState( "forwards", false )
+			setControlState( "left", false )
+			setControlState( "right", false )
+			--setControlState( "sprint", false )
+		end
+	end
+)
