7ef9f5faca3c491010399e532bf36b6ea2a7622e
Socialz <socialz.fin@gmail.com>
2014-12-06 13:49:27 +0200

    admin: new command and getRealPlayerName added

--- admin/server/player_admin.lua
+++ admin/server/player_admin.lua
@@ -20,7 +20,7 @@ addCommandHandler( { "getpos", "pos", "getposition", "getxyz", "getloc", "loc", 
 			x, y, z = math.floor( x * 100 ) / 100, math.floor( y * 100 ) / 100, math.floor( z * 100 ) / 100
 			rotation = math.floor( rotation * 100 ) / 100
 			
-			local playerName = getPlayerName( targetPlayer )
+			local playerName = exports.common:getRealPlayerName( targetPlayer )
 			
 			outputChatBox( ( targetPlayer ~= player and exports.common:formatPlayerName( playerName ) or "Your" ) .. " position:", player, 230, 180, 95, false )
 			outputChatBox( " Position: " .. x .. ", " .. y .. ", " .. z, player, 230, 180, 95, false )
@@ -60,11 +60,48 @@ addCommandHandler( { "makeadmin", "setlevel", "setadminlevel" },
 					triggerClientEvent( targetPlayer, "admin:hideHUD", targetPlayer )
 				end
 				
-				outputChatBox( "Updated " .. exports.common:formatString( getPlayerName( targetPlayer ) ) .. " level to " .. level .. " (" .. levelName .. ").", player, 95, 230, 95, false )
+				outputChatBox( "Updated " .. exports.common:formatString( exports.common:getRealPlayerName( targetPlayer ) ) .. " level to " .. level .. " (" .. levelName .. ").", player, 95, 230, 95, false )
 			else
 				outputChatBox( "Such level does not exist.", player, 230, 95, 95, false )
+			end
+		end
+	end
+)
+
+addCommandHandler( "setskin",
+	function( player, cmd, targetPlayer, skinID )
+		if ( exports.common:isPlayerServerTrialAdmin( player ) ) then
+			if ( not targetPlayer ) or ( not skinID ) then
+				outputChatBox( "SYNTAX: /" .. cmd .. " [partial player name] [skin id]", player, 230, 180, 95, false )
 				
 				return
+			end
+			
+			targetPlayer = exports.common:getPlayerFromPartialName( targetPlayer, player )
+			
+			if ( not targetPlayer ) then
+				outputChatBox( "Could not find a player with that identifier.", player, 230, 95, 95, false )
+				
+				return
+			end
+			
+			local skinID = tonumber( skinID )
+			local foundSkin = false
+			
+			for _, _skinID in ipairs( getValidPedModels( ) ) do
+				if ( _skinID == skinID ) then
+					foundSkin = true
+				end
+			end
+			
+			if ( foundSkin ) then
+				exports.database:query( "UPDATE `characters` SET `skin_id` = ? WHERE `id` = ?", skinID, getElementData( targetPlayer, "database:id" ) )
+				
+				setElementModel( targetPlayer, skinID )
+				
+				outputChatBox( "Updated " .. exports.common:formatString( exports.common:getRealPlayerName( targetPlayer ) ) .. " skin ID to " .. skinID .. ".", player, 95, 230, 95, false )
+			else
+				outputChatBox( "Such skin does not exist.", player, 230, 95, 95, false )
 			end
 		end
 	end
