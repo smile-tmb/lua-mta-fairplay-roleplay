fe8c368a032bce5f606b9bf30a83e9375f850ccd
Socialz <socialz.fin@gmail.com>
2014-12-25 22:28:44 +0200

    realism: fixes and improvements to cks

--- realism/server/character_kill.lua
+++ realism/server/character_kill.lua
@@ -1,32 +1,60 @@
-function characterKill( player, causeOfDeath )
-	exports.database:execute( "UPDATE `characters` SET `is_dead` = '1', `cause_of_death` = ? WHERE `id` = ?", causeOfDeath, exports.common:getCharacterID( player ) )
+function characterKill( characterID, causeOfDeath )
+	local characterPlayer = exports.common:getPlayerByCharacterID( characterID )
 	
-	setElementAlpha( player, 0 )
+	if ( characterPlayer ) then
+		exports.accounts:saveCharacter( characterPlayer )
+	end
 	
-	local ped = createPed( getElementModel( player ), getElementPosition( player ) )
+	local character = exports.accounts:getCharacter( characterID )
 	
-	setElementRotation( ped, getElementRotation( player ) )
-	setElementInterior( ped, getElementInterior( player ) )
-	setElementDimension( ped, getElementDimension( player ) )
+	if ( character ) and ( character.is_dead == 0 ) then
+		exports.database:execute( "UPDATE `characters` SET `is_dead` = '1', `cause_of_death` = ? WHERE `id` = ?", causeOfDeath, characterID )
+		
+		local ped = createPed( character.skin_id, character.pos_x, character.pos_y, character.pos_z, character.rotation, false )
+		
+		setElementInterior( ped, character.interior )
+		setElementDimension( ped, character.dimension )
+		
+		killPed( ped )
+		
+		exports.security:modifyElementData( ped, "npc:character_kill.id", character.id, true )
+		exports.security:modifyElementData( ped, "npc:character_kill.reason", causeOfDeath, true )
+		
+		local player = exports.common:getPlayerByAccountID( character.account )
+		
+		if ( player ) then
+			if ( characterPlayer ) then
+				exports.accounts:characterSelection( player )
+				
+				outputChatBox( "You were character killed by an administrator.", player, 230, 180, 95, false )
+				outputChatBox( " Cause of death: " .. causeOfDeath, player, 230, 180, 95, false )
+			else
+				exports.accounts:updateCharacters( player )
+			end
+		end
+		
+		return true
+	end
 	
-	killPed( ped )
-	
-	exports.security:modifyElementData( ped, "npc:character_kill.id", exports.common:getCharacterID( player ), true )
-	exports.security:modifyElementData( ped, "npc:character_kill.reason", causeOfDeath, true )
-	
-	exports.accounts:characterSelection( player )
+	return false
 end
 
-function characterResurrect( characterName )
-	local character = exports.accounts:getCharacterByName( characterName )
+function characterResurrect( characterID )
+	local character = exports.accounts:getCharacter( characterID )
 	
-	if ( character ) then
+	if ( character ) and ( character.is_dead ~= 0 ) then
 		exports.database:execute( "UPDATE `characters` SET `is_dead` = '0', `cause_of_death` = '' WHERE `id` = ?", character.id )
 		
 		local ped = findCharacterKillPed( character.id )
 		
 		if ( ped ) then
 			destroyElement( ped )
+		end
+		
+		local player = exports.common:getPlayerByAccountID( character.account )
+		
+		if ( player ) and ( not getElementData( player, "player:playing" ) ) then
+			exports.accounts:updateCharacters( player )
 		end
 		
 		return true
@@ -65,3 +93,9 @@ function loadCharacterKills( )
 		exports.security:modifyElementData( ped, "npc:character_kill.reason", data.cause_of_death, true )
 	end
 end
+
+addEventHandler( "onResourceStart", resourceRoot,
+	function( )
+		loadCharacterKills( )
+	end
+)
