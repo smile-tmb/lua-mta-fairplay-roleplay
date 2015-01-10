420d794433e9f464cb11fe9fcf06e75b5e1c77a0
Socialz <socialz.fin@gmail.com>
2015-01-10 13:57:42 +0200

    common: getNearbyElementsByType

--- common/shared/common.lua
+++ common/shared/common.lua
@@ -261,3 +261,15 @@ function getPlayerByCharacterID( characterID )
 	
 	return false
 end
+
+function getNearbyElementsByType( x, y, z, type, distance )
+	local elements = { }
+	
+	for _, element in ipairs( getElementsByType( type ) ) do
+		if ( getDistanceBetweenPoints3D( x, y, z, getElementPosition( element ) ) < distance ) then
+			table.insert( elements, element )
+		end
+	end
+	
+	return elements
+end
