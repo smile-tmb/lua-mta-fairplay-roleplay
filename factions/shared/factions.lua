c762fa17f9193c461321904a2680703442f63a43
Socialz <socialz.fin@gmail.com>
2015-01-12 10:15:45 +0200

    factions: started working on factions a little

--- factions/shared/factions.lua
+++ factions/shared/factions.lua
@@ -22,40 +22,4 @@
 	SOFTWARE.
 ]]
 
-function getFactions( )
-	local factions = { }
-	
-	for _, team in ipairs( getElementsByType( "team" ) ) do
-		if ( exports.factions:getFactionID( team ) ) then
-			table.insert( factions, team )
-		end
-	end
-	
-	return factions
-end
-
-function getFactionByID( id )
-	for _, faction in ipairs( getFactions( ) ) do
-		if ( exports.factions:getFactionID( faction ) == id ) then
-			return faction
-		end
-	end
-	
-	return false
-end
-
-function getFactionByName( name )
-	for _, faction in ipairs( getFactions( ) ) do
-		if ( exports.factions:getFactionName( faction ) == name ) then
-			return faction
-		end
-	end
-	
-	return false
-end
-
-addEventHandler( "onResourceStart", resourceRoot,
-	function( )
-		local civilians = createTeam( "Civilian", 255, 255, 255 )
-	end
-)
+factionRankCount = 20
