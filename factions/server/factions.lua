4343ba8ef4bb2a410202ddb3e47bf6897470127e
Socialz <socialz.fin@gmail.com>
2015-01-12 10:16:06 +0200

    factions: addendum to previous commit

---
+++ factions/server/factions.lua
@@ -0,0 +1,226 @@
+--[[
+	The MIT License (MIT)
+
+	Copyright (c) 2014 Socialz (+ soc-i-alz GitHub organization)
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+]]
+
+local factions = { }
+
+function getFactions( )
+	return factions
+end
+
+function getFactionByID( id )
+	for index, faction in pairs( getFactions( ) ) do
+		if ( faction.id == id ) then
+			return faction, index
+		end
+	end
+	
+	return false
+end
+
+function getFactionByName( name )
+	for index, faction in pairs( getFactions( ) ) do
+		if ( faction.name == name ) then
+			return faction, index
+		end
+	end
+	
+	return false
+end
+
+function createFaction( name )
+	local id = exports.database:insert_id( "INSERT INTO `factions` (name) VALUES (?)", name )
+
+	return id and loadFaction( id ) or false
+end
+
+function deleteFaction( id )
+	local found, index = getFactionByID( id )
+
+	if ( found ) then
+		if ( exports.database:execute( "DELETE FROM `factions` WHERE `id` = ?", id ) ) then
+			factions[ index ] = nil
+
+			exports.database:execute( "DELETE FROM `factions_characters` WHERE `faction_id` = ?", id )
+
+			return true
+		end
+	end
+
+	return false
+end
+
+function addCharacterToFaction( characterID, id, rank, isLeader )
+	if ( not isCharacterInFaction( characterID, id ) ) and ( exports.database:execute( "INSERT INTO `factions_characters` (`character_id`, `faction_id`, `rank`, `is_leader`) VALUES (?, ?, ?, ?)", characterID, id, rank, isLeader ) ) then
+		local faction = getFactionByID( id )
+
+		table.insert( faction.players, { id = characterID, rank = rank, leader = isLeader } )
+
+		for _, data in pairs( faction.players ) do
+			local player = exports.common:getPlayerByCharacterID( data.id )
+
+			if ( player ) then
+				triggerClientEvent( player, "factions:update", player, faction )
+			end
+		end
+
+		return true
+	end
+
+	return false
+end
+
+function addPlayerToFaction( player, id )
+	return addCharacterToFaction( exports.common:getCharacterID( player ), id )
+end
+
+function removeCharacterFromFaction( characterID, id )
+	local index = isCharacterInFaction( characterID, id )
+
+	if ( index ) and ( exports.database:execute( "DELETE FROM `factions_characters` WHERE `character_id` = ? AND `faction_id` = ?", characterID, id ) ) then
+		local faction = getFactionByID( id )
+
+		table.remove( faction.players, index )
+
+		for _, data in pairs( faction.players ) do
+			local player = exports.common:getPlayerByCharacterID( data.id )
+
+			if ( player ) then
+				triggerClientEvent( player, "factions:update", player, faction )
+			end
+		end
+
+		return true
+	end
+
+	return false
+end
+
+function removePlayerFromFaction( player, id )
+	return removeCharacterFromFaction( exports.common:getCharacterID( player ), id )
+end
+
+function isCharacterInFaction( characterID, id, checkForLeadership )
+	local faction = getFactionByID( id )
+
+	if ( faction ) then
+		for index, data in pairs( faction.players ) do
+			if ( data.character_id == characterID ) and ( ( not checkForLeadership ) or ( data.leader ) ) then
+				return index
+			end
+		end
+	end
+
+	return false
+end
+
+function isPlayerInFaction( player, id, checkForLeadership )
+	return isCharacterInFaction( exports.common:getCharacterID( player ), id, checkForLeadership )
+end
+
+function getCharacterFactions( characterID )
+	local query = exports.database:query( "SELECT `faction_id` FROM `factions_characters` WHERE `character_id` = ?", id )
+
+	if ( query ) then
+		local playerFactions = { }
+
+		for _, data in ipairs( query ) do
+			table.insert( playerFactions, data.faction_id )
+		end
+
+		return playerFactions
+	end
+
+	return false
+end
+
+function getPlayerFactions( player, id )
+	return getCharacterFactions( exports.common:getCharacterID( player ) )
+end
+
+function loadFaction( id )
+	local _, index = getFactionByID( id )
+
+	factions[ index ] = nil
+
+	local query = exports.database:query_single( "SELECT * FROM `factions` WHERE `id` = ? LIMIT 1", id )
+
+	if ( query ) then
+		local faction = {
+			id = query.id,
+			name = query.name,
+			motd = query.motd,
+			ranks = { },
+			players = { }
+		}
+
+		for i = 1, factionRankCount do
+			local rank = fromJSON( query[ "rank_" .. i ] )
+
+			faction.ranks[ i ] = {
+				name = rank.name,
+				wage = rank.wage
+			}
+		end
+
+		local players = exports.database:query( "SELECT `character_id` FROM `factions_characters` WHERE `faction_id` = ?", query.id )
+
+		if ( players ) then
+			for _, data in ipairs( players ) do
+				table.insert( faction.players, { id = data.character_id, rank = data.rank, leader = data.is_leader == 1 } )
+
+				local player = exports.common:getPlayerByCharacterID( data.character_id )
+
+				if ( player ) then
+					triggerClientEvent( player, "factions:update", player, faction )
+				end
+			end
+		end
+
+		table.insert( factions, faction )
+
+		return true
+	end
+
+	return false
+end
+
+function loadFactions( )
+	local query = exports.database:query( "SELECT * FROM `factions`" )
+
+	if ( query ) then
+		for _, data in ipairs( query ) do
+			loadFaction( data.id )
+		end
+
+		return true
+	end
+
+	return false
+end
+
+addEventHandler( "onResourceStart", resourceRoot,
+	function( )
+		loadFactions( )
+	end
+)
